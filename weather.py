6908eb2d2f8158a199aba6b539eebab3df7cbed9
zepedaj98 <jayzepeda98@gmail.com>
2023-03-28 11:44:27 -0600

    implemented 10day forecast

--- weather.py
+++ weather.py
@@ -9,6 +9,7 @@ Original file is located at
 
 from bs4 import BeautifulSoup as bs 
 import requests
+
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 # US english
 LANGUAGE = "en-US,en;q=0.5"
@@ -28,6 +29,8 @@ def get_weather_data(url):
 
     # extract region
     result['region'] = soup.find("span", attrs={"data-testid": "PresentationName"}).text
+    # extract date
+    result['date'] = soup.find("span", attrs={"class": "DailyContent--daypartDate--3VGlz"}).text
     #extract sunrise time 
     result['sunrise'] = soup.find("span", attrs={"data-testid": "SunriseTime"}).text
     #extract sunset time 
@@ -35,7 +38,7 @@ def get_weather_data(url):
     # extract temperature now
     result['temp_now'] = soup.find("span", attrs={"data-testid": "TemperatureValue"}).text
     # get the day and hour now
-    result['day'] = soup.find("div", attrs={"class": "DailyForecast--timestamp--22Azh"}).text
+    result['time'] = soup.find("div", attrs={"class": "DailyForecast--timestamp--22Azh"}).text
     # get the precipitation
     result['precipitation'] = soup.find("span", attrs={"data-testid":"precip", "data-testid": "PercentageValue"}).text
     # get the % of humidity
@@ -48,35 +51,45 @@ def get_weather_data(url):
     result['summary'] = soup.find("p", attrs={"data-testid": "wxPhrase"}).text
 
     next_days = []
-    days = soup.find("h3", attrs={"class": "DetailsSummary--daypartName--kbngc"})
-    for day in days.findAll("h3", attrs={"class": "DetailsSummary--daypartName--kbngc"}):
+    days = soup.find("div", attrs={"class": "DailyForecast--DisclosureList--nosQS"})
+    for day in days.findAll("details", attrs={"data-track-string": "detailsExpand"}):
         # extract the name of the day
-        day_name = day.findAll("h3")[0].attrs['daypartName']
-      #  # get weather status for that day
-       # weather = day.find("img").attrs["alt"]
-       # temp = day.findAll("span", {"class": "wob_t"})
-        # maximum temparature in Celsius, use temp[1].text if you want fahrenheit
-       # max_temp = temp[0].text
-        # minimum temparature in Celsius, use temp[3].text if you want fahrenheit
-       # min_temp = temp[2].text
-        #next_days.append({"name": day_name, "weather": weather, "max_temp": max_temp, "min_temp": min_temp})
-        next_days.append({"name": day_name})
+        day_name = day.findAll("h3")[0].text.strip()
+        # get weather status for that day
+        weather = day.find("span", {"class": "DetailsSummary--extendedData--307Ax"}).text.strip()
+        # find min and max temps for 10day forecastboth, both in fahrenheit
+        max_temp = day.find("span", {"class": "DetailsSummary--highTempValue--3PjlX"}).text.strip()
+        min_temp = day.find("span", {"class": "DetailsSummary--lowTempValue--2tesQ"}).text.strip()
+        # find chance of rain
+        rain_chance = day.find("span", {"data-testid": "PercentageValue"}).text.strip()
+        next_days.append({"name": day_name, "max_temp": max_temp, "min_temp": min_temp, "weather": weather, "rain_chance": rain_chance})
     # append to result
     result['next_days'] = next_days
     return result
 
+def print_weather(weather_data):
+    #Print weather data for today
+    print(weather_data["date"],' | ',data["time"][6:])
+    print(weather_data["region"])
+    print(weather_data["temp_now"])
+    print(weather_data["summary"])
+    print(weather_data["wind"])
+    print(weather_data["UV"])
+    print(weather_data["humidity"])
+    print(weather_data["precipitation"])
+    print("Sunrise Time: ", weather_data["sunrise"])
+    print("Sunset Time: ", weather_data["sunset"])
+    #print weather data for 10day forecast
+    for dayweather in weather_data["next_days"]:
+        print("="*40, dayweather["name"], "="*40)
+        print("High:", dayweather["max_temp"], "F")
+        print("Low:", dayweather["min_temp"], "F")
+        print(dayweather["weather"])
+        print(dayweather["rain_chance"])
+
+
+
 if __name__ == "__main__":
     URL = "https://weather.com/weather/tenday/l/Albuquerque+NM?canonicalCityId=2defda56c5089a3cae25463d822f01e81fa91fcc68d08f190e404a38ae70a9f1"
     data = get_weather_data(URL)
-    #Print weather data. 
-    print( data["day"][6:])
-    print(data["region"])
-    print(data["temp_now"])
-    print(data["summary"])
-    print(data["wind"])
-    print(data["UV"])
-    print(data["humidity"])
-    print(data["precipitation"])
-    print(data["sunrise"])
-    print(data["sunset"])
-    print(data["next_days"])
+    print_weather(data)
