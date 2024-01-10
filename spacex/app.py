c666d1cb0b70544f2f44753036dacc39d21bf5eb
zepedaj98 <jayzepeda98@gmail.com>
2024-01-09 19:05:13 -0700

    fix

--- spacex/app.py
+++ spacex/app.py
@@ -45,4 +45,4 @@ app.config["APPLICATION_ROOT"] = "/spacex"
 application = DispatcherMiddleware(Flask("main"), {"/spacex": app})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=<5000>, threaded=True, host='0.0.0.0', request_handler="waitress")
+    app.run(debug=True, port=5000, threaded=True, host='0.0.0.0', request_handler="waitress")
